0d8c854c7954a28f3cc7165791eea8a456b8d6ee
Thalles Leite <eng.thallesleite@gmail.com>
2023-03-13 15:07:16 -0300

    Requisito 01 concluído.

---
+++ src/pages/Album.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Album extends Component {
+  render() {
+    return (
+      <div data-testid="page-album" />
+    );
+  }
+}
+
+Album.propTypes = {
+
+};
+
+export default Album;
